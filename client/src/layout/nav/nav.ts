0ad0975108dcf6b0fceca477fa0472bdc0f16316
tedrobinson <tfqbtd@aol.com>
2026-06-04 14:33:48 -0400

    Problems angular services (49)

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,29 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css',
+})
+export class Nav {
+  private accountService = inject(AccountService);
+  protected creds: any = {};
+  protected loggedIn = signal(false);
+
+  login() {
+    this.accountService.login(this.creds).subscribe({
+      next: (result) => {
+        console.log(result);
+        this.loggedIn.set(true);
+      },
+      error: (error) => alert(error.message),
+    });
+  }
+
+  logout() {
+    this.loggedIn.set(false);
+  }
+}
